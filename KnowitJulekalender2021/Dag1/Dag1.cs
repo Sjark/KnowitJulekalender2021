bd68e4354f50472a42fefc3ac89a614426816831
Lasse Meland <sjark89@gmail.com>
2021-12-02 09:34:15 +0100

    IC

---
+++ KnowitJulekalender2021/Dag1/Dag1.cs
@@ -0,0 +1,58 @@
+﻿namespace KnowitJulekalender2021.Dag1;
+
+public class Dag1 : ISolution
+{
+    private readonly Dictionary<string, int> _dayTranslation = new Dictionary<string, int>()
+    {
+        { "femti", 50 },
+        { "førti", 40 },
+        { "tretti", 30 },
+        { "tjue", 20 },
+        { "nitten", 19 },
+        { "atten", 18 },
+        { "sytten", 17 },
+        { "seksten", 16 },
+        { "femten", 15 },
+        { "fjorten", 14 },
+        { "tretten", 13 },
+        { "tolv", 12 },
+        { "elleve", 11 },
+        { "ti", 10 },
+        { "ni", 9 },
+        { "åtte", 8 },
+        { "sju", 7 },
+        { "seks", 6 },
+        { "fem", 5 },
+        { "fire", 4 },
+        { "tre", 3 },
+        { "to", 2 },
+        { "en", 1 }
+    };
+
+    public void Execute()
+    {
+        var numbers = File.ReadAllText($"{AppContext.BaseDirectory}\\Dag1\\tall.txt").Replace("\n", "").AsSpan();
+
+        var pointer = 0;
+        ReadOnlySpan<char> currentNumbers;
+        var translationKeys = _dayTranslation.Keys;
+        var sum = 0;
+
+        while (pointer < numbers.Length)
+        {
+            currentNumbers = numbers.Slice(pointer);
+
+            foreach (var key in translationKeys)
+            {
+                if (currentNumbers.StartsWith(key))
+                {
+                    sum += _dayTranslation[key];
+                    pointer += key.Length;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
---
+++ KnowitJulekalender2021/Program.cs
@@ -0,0 +1,58 @@
+﻿using KnowitJulekalender2021;
+using System.Reflection;
+
+var days = new Dictionary<string, ISolution>();
+foreach (var day in Assembly.GetExecutingAssembly()
+    .GetTypes()
+    .Where(type => typeof(ISolution).IsAssignableFrom(type) && !type.IsInterface))
+{
+    var instance = (ISolution?)Activator.CreateInstance(day);
+
+    if (instance != null)
+    {
+        days.Add(day.Name, instance);
+    }
+}
+
+while (true)
+{
+    Console.WriteLine("Please select a day to run:");
+    Console.WriteLine();
+
+    var i = 1;
+
+    var daysSorted = days.Keys.OrderByAlphaNumeric(a => a).ToList();
+
+    foreach (var day in days.Keys.OrderByAlphaNumeric(a => a))
+    {
+        Console.WriteLine($"{i}: {day}");
+
+        i++;
+    }
+
+    Console.WriteLine();
+    Console.Write("Enter day: ");
+    var selectedDay = Console.ReadLine();
+    int selectedDayNum;
+
+    while (!int.TryParse(selectedDay, out selectedDayNum) || selectedDayNum - 1 < 0 || selectedDayNum > daysSorted.Count)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Invalid day entered");
+        Console.Write("Enter day: ");
+        selectedDay = Console.ReadLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"Running {daysSorted[selectedDayNum - 1]}, result:");
+    Console.WriteLine();
+
+    days[daysSorted[selectedDayNum - 1]].Execute();
+
+    Console.WriteLine();
+    Console.WriteLine("Press <enter> to get back to main menu");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+
