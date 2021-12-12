7140139b1f5ff00bbd12a78aefc526a07b130759
Lasse Meland <sjark89@gmail.com>
2021-12-12 14:17:25 +0100

    Dag 6 og 11

---
+++ KnowitJulekalender2021/Dag11/Dag11.cs
@@ -0,0 +1,113 @@
+﻿namespace KnowitJulekalender2021.Dag11;
+
+public class Dag11 : ISolution
+{
+    public void Execute()
+    {
+        var names = File.ReadAllLines($"{AppContext.BaseDirectory}\\Dag11\\names.txt");
+
+        var namesWithErrors = new Dictionary<string, List<string>>();
+        var gifts = new Dictionary<string, int>();
+
+        foreach (var name in names)
+        {
+            namesWithErrors.Add(name, new List<string> { name });
+            gifts.Add(name, 0);
+
+            for (int i = 0; i < name.Length - 1; i++)
+            {
+                var tempName = name.ToCharArray();
+                var secondChar = name[i + 1];
+
+                tempName[i + 1] = tempName[i];
+                tempName[i] = secondChar;
+
+                namesWithErrors[name].Add(new string(tempName));
+            }
+        }
+
+        var locked = File.ReadAllLines($"{AppContext.BaseDirectory}\\Dag11\\locked.txt");
+
+        foreach (var phrase in locked)
+        {
+            string? nameFound = null;
+            var excessCharacters = int.MaxValue;
+            var excessCharactersSet = new HashSet<int>();
+
+            foreach (var name in namesWithErrors.Keys)
+            {
+                var match = IsMatch(phrase, namesWithErrors[name]);
+
+                if (match.Found)
+                {
+                    if (excessCharactersSet.Contains(match.ExcessCharacters) && match.ExcessCharacters == excessCharacters)
+                    {
+                        nameFound = null;
+                        continue;
+                    }
+
+                    excessCharactersSet.Add(match.ExcessCharacters);
+
+                    if (match.ExcessCharacters < excessCharacters)
+                    {
+                        nameFound = name;
+                        excessCharacters = match.ExcessCharacters;
+                    }
+                }
+            }
+
+            if (nameFound != null)
+            {
+                gifts[nameFound]++;
+            }
+        }
+
+        var bestKid = gifts.OrderByDescending(a => a.Value).First();
+
+        Console.WriteLine($"{bestKid.Key},{bestKid.Value}");
+    }
+
+    public (bool Found, int ExcessCharacters) IsMatch(string phrase, List<string> names)
+    {
+        bool found = false;
+        var excessCharacters = int.MaxValue;
+        foreach (var name in names)
+        {
+            var currentChar = 0;
+            var firstCharIndex = -1;
+            var lastCharIndex = -1;
+
+            for (var i = 0; i < phrase.Length; i++)
+            {
+                if (phrase[i] == name[currentChar])
+                {
+                    currentChar++;
+
+                    if (firstCharIndex == -1)
+                    {
+                        firstCharIndex = i;
+                    }
+
+                    if (currentChar == name.Length)
+                    {
+                        lastCharIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            if (lastCharIndex != -1)
+            {
+                var tempExcess = lastCharIndex - firstCharIndex - name.Length + 1;
+
+                if (tempExcess < excessCharacters)
+                {
+                    excessCharacters = tempExcess;
+                    found = true;
+                }
+            }
+        }
+
+        return (found, excessCharacters);
+    }
+}
---
+++ KnowitJulekalender2021/Dag6/Dag6.cs
@@ -0,0 +1,86 @@
+﻿namespace KnowitJulekalender2021.Dag6;
+
+public class Dag6 : ISolution
+{
+    public void Execute()
+    {
+        var presents = File.ReadAllLines($"{AppContext.BaseDirectory}\\Dag6\\pakker.txt");
+        var presentsGrid = new bool[21, presents.Length];
+        var fallenPresents = 0;
+
+        foreach (var present in presents)
+        {
+            var presentSize = present.Split(',').Select(a => int.Parse(a)).ToList();
+
+            var yPos = 0;
+            var foundY = false;
+
+            while (!foundY)
+            {
+                var isEmpty = true;
+
+                for (int i = presentSize[0]; i < (presentSize[0] + presentSize[1]); i++)
+                {
+                    if (presentsGrid[i, yPos])
+                    {
+                        isEmpty = false;
+                        break;
+                    }
+                }
+
+                if (isEmpty)
+                {
+                    foundY = true;
+                }
+                else
+                {
+                    yPos++;
+                }
+            }
+
+            if (yPos == 0)
+            {
+                for (int i = presentSize[0]; i < (presentSize[0] + presentSize[1]); i++)
+                {
+                    presentsGrid[i, yPos] = true;
+                }
+            }
+            else
+            {
+                var half = presentSize[1] / 2;
+                var extra = presentSize[1] % 2 == 1 ? 1 : 0;
+
+                var leftHalf = false;
+                var rightHalf = false;
+
+                for (int i = presentSize[0]; i < (presentSize[0] + presentSize[1]); i++)
+                {
+                    if (i < (presentSize[0] + half + extra) && presentsGrid[i, yPos - 1])
+                    {
+                        leftHalf = true;
+                    }
+
+                    
+                    if (i >= (presentSize[0] + presentSize[1] - half - extra) && presentsGrid[i, yPos - 1])
+                    {
+                        rightHalf = true;
+                    }
+                }
+
+                if (leftHalf && rightHalf)
+                {
+                    for (int i = presentSize[0]; i < (presentSize[0] + presentSize[1]); i++)
+                    {
+                        presentsGrid[i, yPos] = true;
+                    }
+                }
+                else
+                {
+                    fallenPresents++;
+                }
+            }
+        }
+
+        Console.WriteLine(fallenPresents);
+    }
+}
