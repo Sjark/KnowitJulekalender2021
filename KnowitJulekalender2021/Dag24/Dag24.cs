a6cb3e3ca2bd4d3db4fe1fc2e606a8b33cef84a1
Lasse Meland <sjark89@gmail.com>
2021-12-24 13:42:11 +0100

    Parsing of Dag 24

---
+++ KnowitJulekalender2021/Dag24/Dag24.cs
@@ -0,0 +1,37 @@
+﻿namespace KnowitJulekalender2021.Dag24;
+
+public class Dag24 : ISolution
+{
+    public void Execute()
+    {
+        var feilregistreringer = File.ReadAllLines($"{AppContext.BaseDirectory}\\Dag24\\feilregistreringer.txt")
+            .Select(a => new KeyValuePair<string, List<long>>(a.Split("/").First(), a.Split("/").Skip(1).Select(a => long.Parse(a.Trim())).ToList()))
+            .ToDictionary(a => a.Key, a => a.Value);
+
+        var skritt = File.ReadAllLines($"{AppContext.BaseDirectory}\\Dag24\\skritt.txt");
+
+        var weeks = new List<Dictionary<Worker, List<long>>>();
+
+        var currentWeek = new Dictionary<Worker, List<long>>();
+
+        foreach (var line in skritt)
+        {
+            if (line == "##")
+            {
+                weeks.Add(currentWeek);
+                currentWeek = new Dictionary<Worker, List<long>>();
+                continue;
+            }
+
+            var splittedLine = line.Split("/");
+            currentWeek.Add(new Worker(splittedLine[0].Trim(), splittedLine[1].Trim()), splittedLine.Skip(2).Select(a => string.IsNullOrWhiteSpace(a) ? 0L : long.Parse(a.Trim())).ToList());
+        }
+
+        if (currentWeek.Count > 0)
+        {
+            weeks.Add(currentWeek);
+        }
+    }
+}
+
+public record Worker(string Name, string Work);
