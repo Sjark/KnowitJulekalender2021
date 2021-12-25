f69f58dddcc69dc1112cce9ead8e8a739ea1612a
Lasse Meland <sjark89@gmail.com>
2021-12-25 13:26:50 +0100

    Dag 24

--- KnowitJulekalender2021/Dag24/Dag24.cs
+++ KnowitJulekalender2021/Dag24/Dag24.cs
@@ -1,11 +1,13 @@
-﻿namespace KnowitJulekalender2021.Dag24;
+﻿using System.Text;
+
+namespace KnowitJulekalender2021.Dag24;
 
 public class Dag24 : ISolution
 {
     public void Execute()
     {
         var feilregistreringer = File.ReadAllLines($"{AppContext.BaseDirectory}\\Dag24\\feilregistreringer.txt")
-            .Select(a => new KeyValuePair<string, List<long>>(a.Split("/").First(), a.Split("/").Skip(1).Select(a => long.Parse(a.Trim())).ToList()))
+            .Select(a => new KeyValuePair<string, List<long>>(a.Split("/").First().Trim(), a.Split("/").Skip(1).Select(a => long.Parse(a.Trim())).ToList()))
             .ToDictionary(a => a.Key, a => a.Value);
 
         var skritt = File.ReadAllLines($"{AppContext.BaseDirectory}\\Dag24\\skritt.txt");
@@ -31,6 +33,60 @@ public class Dag24 : ISolution
         {
             weeks.Add(currentWeek);
         }
+
+        foreach (var week in weeks)
+        {
+            foreach (var worker in week)
+            {
+                var feil = feilregistreringer[worker.Key.Work];
+
+                if (feil != null)
+                {
+                    for (int i = 0; i < worker.Value.Count; i++)
+                    {
+                        if (worker.Value[i] != 0)
+                        {
+                            worker.Value[i] -= feil[i];
+                        }
+                        if (worker.Value[i] < 0)
+                        {
+                            worker.Value[i] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        var nissen = weeks.Select(a => a.Where(b => b.Key.Name == "Nissen 🎅").Single().Value)
+            .ToList();
+
+        var alvene = weeks.Select(a => a.Where(b => b.Key.Name != "Nissen 🎅")).ToList();
+
+        var nissenAvg = new List<double>();
+
+        for (int i = 0; i < 7; i++)
+        {
+            nissenAvg.Add((double)nissen.Select(a => a[i]).Sum() / nissen.Count);
+        }
+
+        var alveneAvg = new List<long>() { 0, 0, 0, 0, 0, 0, 0 };
+
+        foreach (var alv in alvene)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                alveneAvg[i] += alv.Select(a => a.Value[i]).Sum();
+            }
+        }
+
+        var result = new StringBuilder();
+
+        for (int i = 0; i < 7; i++)
+        {
+            result.Append(Math.Floor(nissenAvg[i] - ((double)alveneAvg[i] / weeks.Count / alvene.First().Count())));
+        }
+
+        Console.WriteLine(result.ToString());
     }
 }
 
